a411c643f30f5e7125579d23b060410513a4521c
zikei <sigetaka1517@gmail.com>
2024-05-01 22:28:01 +0900

    add SecurityConfig no login

---
+++ src/main/kotlin/com/example/todo/ui/config/WebSecurityConfig.kt
@@ -0,0 +1,24 @@
+package com.example.todo.ui.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        http{
+            authorizeHttpRequests{
+                authorize("/**", permitAll)
+            }
+        }
+        return http.build()
+    }
+}
