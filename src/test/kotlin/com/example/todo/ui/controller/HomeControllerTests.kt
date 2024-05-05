49bf6d89f18e221a7818b6b7d6498cb5acdc1fa4
zikei <sigetaka1517@gmail.com>
2024-05-05 23:14:32 +0900

    change HomeControllerTests

--- src/test/kotlin/com/example/todo/ui/controller/HomeControllerTests.kt
+++ src/test/kotlin/com/example/todo/ui/controller/HomeControllerTests.kt
@@ -1,14 +1,26 @@
 package com.example.todo.ui.controller
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.servlet.view.InternalResourceViewResolver
 
 @SpringBootTest
 class HomeControllerTests {
     private val homeController = HomeController()
-    private val mockMvc = MockMvcBuilders.standaloneSetup(homeController).build()
+    private lateinit var mockMvc : MockMvc
+
+    @BeforeEach
+    fun setup() {
+        val viewResolver = InternalResourceViewResolver()
+        viewResolver.setPrefix("/WEB-INF/views/")
+        viewResolver.setSuffix(".html")
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).setViewResolvers(viewResolver).build()
+    }
+
 
     @Test
     fun `home page is success`() {
