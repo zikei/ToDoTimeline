23ff76872b64e1735cf451db1e22475d8fe2aa56
zikei <sigetaka1517@gmail.com>
2024-05-06 12:47:56 +0900

    WebSecurityConfig Profile

--- src/main/kotlin/com/example/todo/ui/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/todo/ui/config/WebSecurityConfig.kt
@@ -2,12 +2,13 @@ package com.example.todo.ui.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 
-
+@Profile("default")
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
@@ -16,7 +17,22 @@ class WebSecurityConfig {
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
         http{
             authorizeHttpRequests{
-                authorize("/**", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+        }
+        return http.build()
+    }
+}
+
+@Profile("div")
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfigNoAuth {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        http{
+            authorizeHttpRequests{
+                authorize(anyRequest, permitAll)
             }
         }
         return http.build()
