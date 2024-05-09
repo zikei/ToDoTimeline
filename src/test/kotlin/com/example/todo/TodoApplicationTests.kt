cca8f9655352a8f4faf6b9d91a53de18e3241585
zikei <sigetaka1517@gmail.com>
2024-05-10 06:43:39 +0900

    add test `logged in user can access todo page`

--- src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithAnonymousUser
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -25,6 +26,15 @@ class TodoApplicationTests(@Autowired private val mockMvc: MockMvc) {
 				header {
 					redirectedUrlPattern("**/login")
 				}
+			}
+	}
+
+	@Test
+	@WithMockUser(authorities = ["USER"])
+	fun `logged in user can access todo page`() {
+		mockMvc.get("/todo")
+			.andExpect {
+				status { isOk() }
 			}
 	}
 
