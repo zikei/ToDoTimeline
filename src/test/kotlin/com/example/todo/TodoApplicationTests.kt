19585e8a494a4f521291381aa05b6c301f4e8dd1
zikei <sigetaka1517@gmail.com>
2024-05-10 06:27:07 +0900

    add tests home,login page is not protected

--- src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -1,13 +1,36 @@
 package com.example.todo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.context.support.WithAnonymousUser
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 @SpringBootTest
-class TodoApplicationTests {
+@AutoConfigureMockMvc
+class TodoApplicationTests(@Autowired private val mockMvc: MockMvc) {
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	@WithAnonymousUser
+	fun `home page is not protected`() {
+		mockMvc.get("/")
+			.andExpect {
+				status { isOk() }
+			}
+	}
+
+	@Test
+	@WithAnonymousUser
+	fun `login page is not protected`() {
+		mockMvc.get("/login")
+			.andExpect {
+				status { isOk() }
+			}
+	}
 }
