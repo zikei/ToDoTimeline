d615b5036a24fa639693cbb01bac5db247c43a42
zikei <sigetaka1517@gmail.com>
2024-05-09 16:05:29 +0900

    add fun todo page tests

--- src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -1,13 +1,31 @@
 package com.example.todo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.context.support.WithAnonymousUser
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 @SpringBootTest
-class TodoApplicationTests {
+@AutoConfigureMockMvc
+class TodoApplicationTests(@Autowired private val mockMvc: MockMvc) {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	@WithAnonymousUser
+	fun `todo page redirects to login`() {
+		mockMvc.get("/todo")
+			.andExpect {
+				status { is3xxRedirection() }
+				header {
+					redirectedUrlPattern("**/login")
+				}
+			}
 	}
 
 }
