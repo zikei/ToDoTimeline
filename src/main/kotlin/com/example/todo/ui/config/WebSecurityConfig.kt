62c9ba180dc1ca1d1180cc23451154996c76ecad
zikei <sigetaka1517@gmail.com>
2024-05-16 16:01:01 +0900

    add NoAuth User

--- src/main/kotlin/com/example/todo/ui/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/todo/ui/config/WebSecurityConfig.kt
@@ -1,6 +1,8 @@
 package com.example.todo.ui.config
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 
 @Profile("default")
@@ -49,14 +54,37 @@ class WebSecurityConfig(@Autowired val userDetailsService: UserDetailsService) {
 @Profile("dev")
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfigNoAuth {
+class WebSecurityConfigNoAuth(@Autowired val userDetailsService: UserDetailsService) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
         http{
             authorizeHttpRequests{
-                authorize(anyRequest, permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            formLogin {
+                loginPage = "/dev/login"
+                permitAll = true
             }
         }
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
+
+@Profile("dev")
+@Controller
+@RequestMapping("/dev")
+class DevLoginController(
+    @Value("\${dev.user.name}") private val userName: String,
+    @Value("\${dev.user.pass}") private val userPass: String
+){
+    @GetMapping("/login")
+    fun login(request: HttpServletRequest): String{
+        request.login(userName, userPass)
+        return "redirect:/"
     }
 }
