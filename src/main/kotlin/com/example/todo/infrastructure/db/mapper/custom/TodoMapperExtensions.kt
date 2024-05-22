263fdaa0203a3facbcf4107ab2e03e0ac230433d
zikei <sigetaka1517@gmail.com>
2024-05-22 17:03:05 +0900

    add TodoMapper

---
+++ src/main/kotlin/com/example/todo/infrastructure/db/mapper/custom/TodoMapperExtensions.kt
@@ -0,0 +1,45 @@
+package com.example.todo.infrastructure.db.mapper.custom
+
+import com.example.todo.infrastructure.db.mapper.custom.ParentTaskDynamicSqlSupport.PTask
+import com.example.todo.infrastructure.db.mapper.TaskDynamicSqlSupport.Task
+import com.example.todo.infrastructure.db.record.custom.TodoRecord
+import org.mybatis.dynamic.sql.SqlBuilder.*
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.from
+
+private val columnList = listOf(
+    Task.taskid,
+    Task.userid,
+    Task.taskname,
+    Task.createdate,
+    Task.severity,
+    Task.deadline,
+    Task.taskstatus,
+
+    PTask.taskid.`as`("ptaskid"),
+    PTask.userid.`as`("puserid"),
+    PTask.taskname.`as`("ptaskname"),
+    PTask.createdate.`as`("pcreatedate"),
+    PTask.severity.`as`("pseverity"),
+    PTask.deadline.`as`("pdeadline"),
+    PTask.taskstatus.`as`("ptaskstatus")
+
+)
+
+fun TodoMapper.select(): List<TodoRecord> {
+    val selectStatement = select(columnList).from(Task) {
+        leftJoin(PTask, "p") {
+            on(Task.parentid, equalTo(PTask.taskid))
+        }
+    }
+    return selectMany(selectStatement)
+}
+
+fun TodoMapper.selectByPrimaryKey(id_: Int): TodoRecord? {
+    val selectStatement = select(columnList).from(Task) {
+        leftJoin(PTask, "p") {
+            on(Task.parentid, equalTo(PTask.taskid))
+        }
+        where(Task.taskid, isEqualTo(id_))
+    }
+    return selectOne(selectStatement)
+}
