5bffbb0d430a25ba44946692386c1c9b125aeb22
zikei <sigetaka1517@gmail.com>
2024-06-01 20:48:04 +0900

    add TaskStatusResponse

--- src/main/kotlin/com/example/todo/domain/enums/TaskStatus.kt
+++ src/main/kotlin/com/example/todo/domain/enums/TaskStatus.kt
@@ -6,6 +6,8 @@ package com.example.todo.domain.enums
  * closed:完了
  * cancel:中止
  */
-enum class TaskStatus {
-    open, closed, cancel
+enum class TaskStatus(val status: String, val statusName: String) {
+    `open`("open","進行中"),
+    closed("closed","完了"),
+    cancel("cancel","中止")
 }
--- src/main/kotlin/com/example/todo/ui/form/TodoFrom.kt
+++ src/main/kotlin/com/example/todo/ui/form/TodoFrom.kt
@@ -2,10 +2,17 @@ package com.example.todo.ui.form
 
 import com.example.todo.domain.enums.TaskStatus
 import com.example.todo.domain.model.Todo
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
 import java.time.LocalDate
 
 /** ToDo一覧返却 */
 data class GetTodoListResponse(val todoList: List<TodoInfo>)
+
+/** Todo詳細返却 */
+data class GetTodoDetailResponse(val todo: TodoInfo)
+
 
 /** ToDo情報 */
 data class TodoInfo(
@@ -15,10 +22,17 @@ data class TodoInfo(
     val createDate: LocalDate,
     val severity: String,
     val deadline: LocalDate?,
-    val taskStatus: TaskStatus,
+    val taskStatus: TaskStatusResponse,
     val parentId: Int?,
     val parentName: String?
 ){
     constructor(todo: Todo): this(todo.taskId, todo.userId, todo.taskName, todo.createDate, todo.severity.severityName,
-        todo.deadline, todo.taskStatus, todo.parent?.taskId, todo.parent?.taskName)
+        todo.deadline, TaskStatusResponse(todo.taskStatus), todo.parent?.taskId, todo.parent?.taskName)
+}
+
+data class TaskStatusResponse(
+    val status: String,
+    val statusName: String
+){
+    constructor(taskStatus: TaskStatus): this(taskStatus.status, taskStatus.statusName)
 }
