b9765330309e73f4399ddca17883b4f6330a2668
zikei <sigetaka1517@gmail.com>
2024-06-30 18:46:37 +0900

    add TimelinePage

--- src/main/kotlin/com/example/todo/ui/controller/TimelineController.kt
+++ src/main/kotlin/com/example/todo/ui/controller/TimelineController.kt
@@ -1,9 +1,11 @@
 package com.example.todo.ui.controller
 
 import com.example.todo.domain.service.TimelineService
+import com.example.todo.ui.form.RegisterPostRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 class TimelineController(
     @Autowired private val tlService: TimelineService
 ) {
+    @ModelAttribute
+    fun setupRegisterPostRequest(): RegisterPostRequest{
+        return RegisterPostRequest()
+    }
+
     /** タイムライン */
     @GetMapping
     fun timeline(): String{
